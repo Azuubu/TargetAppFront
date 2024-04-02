e017ed1774b589c0daec01245aaa7ee97f6d9197
Bartlomiej Mroczkowski <bartlomiej.mroczkowski77@gmail.com>
2024-04-02 02:32:53 +0200

    test

---
+++ targetappfront/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import MainPage from './pages/HomePage';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <></>;
+}
+
+export default App;
